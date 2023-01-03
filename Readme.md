41791f90456c9d56c4763d4fdcb7893f7dfb1d7a
gautamjuyal <gautam.juyal.94@gmail.com>
2023-01-03 19:04:16 +0530

    initial build

---
+++ Readme.md
@@ -0,0 +1,2 @@
+<h2>Mini Music Player<h2> built using HTML, SCSS and JavaScript. The project uses HTML audio tag for audio playback.
+The project is based on one of Vanilla JavaScript projects by (Brad Traversy)[].
---
+++ script.js
@@ -0,0 +1,67 @@
+const player = document.querySelector(".player");
+const albumArt = document.querySelector(".img-container img");
+const songTitle = document.querySelector(".song-title");
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress-bar");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const audio = document.getElementById("audio");
+
+// data
+
+const songTitles = [
+  "Price of freedom",
+  "Relaxed Vlog",
+  "The Beat of Nature",
+  "Ukulele Trip",
+];
+
+//default song
+
+let songIndex = 0;
+
+const loadSong = (n) => {
+  let songName = songTitles[n];
+  audio.src = `Music/${songName}.mp3`;
+  albumArt.setAttribute("src", `./images/${songName}.jpg`);
+  songTitle.innerText = songName;
+};
+
+loadSong(songIndex);
+
+//functions
+
+let playing;
+
+const playBtnHandler = () => {
+  if (playing === true) {
+    player.classList.remove("play");
+    audio.pause();
+    playing = false;
+    return;
+  }
+  player.classList.add("play");
+  audio.play();
+  playing = true;
+};
+
+const nextBtnHandler = () => {
+  songIndex++;
+  if (songIndex > songTitles.length - 1) songIndex = 0;
+  loadSong(songIndex);
+  audio.play();
+};
+
+const prevBtnHandler = () => {
+  songIndex--;
+  if (songIndex < 0) songIndex = songTitles.length - 1;
+  loadSong(songIndex);
+  audio.play();
+};
+
+// event listeners
+
+playBtn.addEventListener("click", playBtnHandler);
+
+nextBtn.addEventListener("click", nextBtnHandler);
